baf87490d1de92464ef2bf552b6a81d86c58a48f
go-laoji <us.hipeace86@gmail.com>
2023-11-12 10:48:51 +0800

    use_symlinks=False

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from huggingface_hub import snapshot_download
 import os
 
-snapshot_download(repo_id=os.getenv('repo_id'),local_dir="./models")
+snapshot_download(
+    repo_id=os.getenv('repo_id'),
+    local_dir_use_symlinks=False,
+    local_dir="./models")
